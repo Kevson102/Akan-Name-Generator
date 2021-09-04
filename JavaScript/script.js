00ed1f8757d9008aebebbf398e415a9a9d23b525
Kevson102 <nyambura.njuguna@student.moringaschool.com>
2021-09-04 10:22:51 +0300

    creates a variable that reads the gender details entered by the user in the html page

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -5,8 +5,11 @@ function akanGenerator (){
   var month = document.getElementById('month').value;
   var yearOfBirth = document.getElementById('year').value;
 
+  var genders = document.getElementById('gender');
+  var gender = genders.options[genders.selectedIndex].text;
 
 
+  
 }
 
 ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD )
