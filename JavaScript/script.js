2ee97284b6c29d4cfa69ea420003fbb9f6f19743
Kevson102 <nyambura.njuguna@student.moringaschool.com>
2021-09-04 10:46:48 +0300

    creates a variable for the days of the week and creates a control flow that issues the Akan names based on gender

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -14,13 +14,19 @@ function akanGenerator (){
   // The month and the date require no modification.
 
   // We now create arrays to hold the male Akan names and the female Akan names.
-  var nameNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+  var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
   var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+  var weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
   // With all the variables that we need, we can now write the formula for calculating the day of the week
   var dayOfWeek = (((century/4)-2*century-1)+((5-year/4))+((26*(month+1)/10))+date) % 7;
   // since the day of week cannot be a decimal, we round down any decimal number to the nearest whole number.
   var dayOfWeek = Math.floor(dayOfWeek);
-}
 
-( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD )
+  // Since the Akan names are given based on gender, we create a control flow.
+  if (gender === "Male"){
+    document.getElementById('output').innerHTML = "Since you are "+ gender+" and you were born on "+weekDays[dayOfWeek]+", your Akan name is "+maleNames[dayOfWeek]+".";
+  } else {
+    document.getElementById('output').innerHTML = "Since you are "+ gender+" and you were born on "+weekDays[dayOfWeek]+", your Akan name is "+femaleNames[dayOfWeek]+".";
+  }
+}
