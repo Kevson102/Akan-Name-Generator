eb128b795bbfa16a8e2689d09e33b6fcf425200b
Kevson102 <nyambura.njuguna@student.moringaschool.com>
2021-09-04 10:21:58 +0300

    creates variables that read the date information entered by the user in the html page

---
+++ JavaScript/script.js
@@ -0,0 +1,12 @@
+function akanGenerator (){
+  // We begin by collecting the form inputs.
+
+  var date = document.getElementById('date').value;
+  var month = document.getElementById('month').value;
+  var yearOfBirth = document.getElementById('year').value;
+
+
+
+}
+
+( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD )
